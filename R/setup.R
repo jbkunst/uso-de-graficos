366916109a2044588cf7c7c5fed208f787b91da6
Joshua <jbkunst@gmail.com>
2018-01-08 18:56:08 -0300

    yuju

--- R/setup.R
+++ R/setup.R
@@ -1,4 +1,21 @@
 library(tidyverse)
+library(viridis)
+library(scales)
+library(hrbrthemes)
+
+theme_set(
+  theme_ipsum(
+    base_family = "Roboto Condensed",
+    plot_title_face = "plain",
+    plot_title_size = "plain",
+    plot_margin = margin(5, 5, 5, 5)) +
+    theme(
+      title = element_text(colour = "#444444"),
+      panel.grid.major = element_line(colour = "grey90"),
+      panel.grid.minor = element_line(colour = "grey90"),
+      legend.position = "bottom"
+    )
+)
 
 knitr::opts_chunk$set(
   message = FALSE,
