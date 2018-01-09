2651adfe5d5bb13aa7887ee1c403bb9e79ccb6b1
Joshua <jbkunst@gmail.com>
2018-01-09 17:50:04 -0300

    adding 90%

--- R/setup.R
+++ R/setup.R
@@ -17,10 +17,28 @@ theme_set(
     )
 )
 
+theme_null <- function(...) {
+  theme(...,
+        axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position = "none",
+        panel.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        plot.background=element_blank())
+}
+
 knitr::opts_chunk$set(
   message = FALSE,
   warning = FALSE,
-  echo = TRUE,
+  echo = FALSE,
   dev = "svg",
-  cache = TRUE
+  cache = TRUE,
+  cache.path = ".cache/",
+  fig.path = "img/"
 )
