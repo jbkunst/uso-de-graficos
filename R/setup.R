1135777621e0b988c497a5fb6f563330177e4268
jbkunst <jbkunst@gmail.com>
2018-01-10 20:01:55 -0300

    adding 2nd presentation

--- R/setup.R
+++ R/setup.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(viridis)
 library(scales)
 library(hrbrthemes)
+library(gridExtra)
+library(ggbeeswarm)
 
 theme_set(
   theme_ipsum(
@@ -42,3 +44,37 @@ knitr::opts_chunk$set(
   cache.path = ".cache/",
   fig.path = "img/"
 )
+
+# datasets ----------------------------------------------------------------
+n <- 3500
+
+dfdist <- bind_rows(
+  data_frame(key = "Simetrica", value = rnorm(n)),
+  data_frame(key = "Bimodal", value =  rnorm(n) + ifelse(runif(n) < 0.45, 4, 0)),
+  data_frame(key = "Uniforme", value =  runif(n)),
+  data_frame(key = "Asimetrica", value = rchisq(n, 3))
+) %>%
+  mutate(key = factor(key, levels = unique(key))) %>% 
+  group_by(key) %>% 
+  mutate(value = (value - min(value))/(max(value) - min(value))) %>% 
+  ungroup()
+
+dfstats <- dfdist %>% 
+  group_by(key) %>% 
+  summarise(media = mean(value), mediana = median(value)) %>% 
+  gather(stat, value, -key)
+
+gg_dists <- function(k = "Simetrica", color = "#673AB7") {
+  
+  dfdist2 <- dfdist %>% 
+    filter(key == k) %>% 
+    sample_n(1500)
+  gg <- ggplot(dfdist2) + coord_flip() + theme_null()
+  
+  grid.arrange(
+    ggplot(dfdist2) + geom_histogram(aes(value), fill = color) + theme_null() + ggtitle("Histograma"),
+    gg + geom_beeswarm(aes(x = factor(1), y = value), color = color, cex = 1.1, alpha = 0.2) + ggtitle("Beeswarm"),
+    gg + geom_boxplot(aes(x = factor(1), y = value), color = color) + ggtitle("Boxplot"),
+    gg + geom_violin(aes(x = factor(1), y = value), color = color) + ggtitle("Violin")
+  )
+}
